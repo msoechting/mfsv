b54f96a12e157e4d4ae6d3f5f3b73b9ef0b2b7fd
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2016-06-07 18:33:04 +0200

    Added debug options for AA. Added alternative model (mitsuba).

--- js/mfsv.js
+++ js/mfsv.js
@@ -9,23 +9,6 @@ window.onload = function() {
 
 function MFSViewer(targetDiv, width, height, objPath) {
 	var myself = this;
-	var kernel = [
-		151, 160, 137,  91,  90,  15, 131,  13, 201,  95,  96,  53, 194, 233,   7, 225,
-    140,  36, 103,  30,  69, 142,   8,  99,  37, 240,  21,  10,  23, 190,   6, 148,
-    247, 120, 234,  75,   0,  26, 197,  62,  94, 252, 219, 203, 117,  35,  11,  32,
-     57, 177,  33,  88, 237, 149,  56,  87, 174,  20, 125, 136, 171, 168,  68, 175,
-     74, 165,  71, 134, 139,  48,  27, 166,  77, 146, 158, 231,  83, 111, 229, 122,
-     60, 211, 133, 230, 220, 105,  92,  41,  55,  46, 245,  40, 244, 102, 143,  54,
-     65,  25,  63, 161,   1, 216,  80,  73, 209,  76, 132, 187, 208,  89,  18, 169,
-    200, 196, 135, 130, 116, 188, 159,  86, 164, 100, 109, 198, 173, 186,   3,  64,
-     52, 217, 226, 250, 124, 123,   5, 202,  38, 147, 118, 126, 255,  82,  85, 212,
-    207, 206,  59, 227,  47,  16,  58,  17, 182, 189,  28,  42, 223, 183, 170, 213,
-    119, 248, 152,   2,  44, 154, 163,  70, 221, 153, 101, 155, 167,  43, 172,   9,
-    129,  22,  39, 253,  19,  98, 108, 110,  79, 113, 224, 232, 178, 185, 112, 104,
-    218, 246,  97, 228, 251,  34, 242, 193, 238, 210, 144,  12, 191, 179, 162, 241,
-     81,  51, 145, 235, 249,  14, 239, 107,  49, 192, 214,  31, 181, 199, 106, 157,
-    184,  84, 204, 176, 115, 121,  50,  45, 127,   4, 150, 254, 138, 236, 205,  93,
-    222, 114,  67,  29,  24,  72, 243, 141, 128, 195,  78,  66, 215,  61, 156, 180];
 
 	this.animate = function() {
 		requestAnimationFrame(myself.animate);
@@ -48,10 +31,10 @@ function MFSViewer(targetDiv, width, height, objPath) {
 
 	this.render = function() {
 		if (this.mainSceneShaderMaterial.uniforms.antiAliasing.value) {
-			var xRand = kernel[this.frameCount % 255] / 255 - 0.5;
-			var yRand = kernel[this.frameCount % 255] / 255 - 0.5; //1*(Math.random() - 0.5);
-			this.mainSceneShaderMaterial.uniforms.aaNdcOffset.value.x = xRand / this.width;
-			this.mainSceneShaderMaterial.uniforms.aaNdcOffset.value.y = yRand / this.height;
+			var xRand = Math.random() - 0.5;//kernel[this.frameCount % 255] / 255 - 0.5;
+			var yRand = Math.random() - 0.5;//kernel[this.frameCount % 255] / 255 - 0.5; //1*(Math.random() - 0.5);
+			this.mainSceneShaderMaterial.uniforms.aaNdcOffset.value.x = 2*this.aaNDCOffset*xRand / this.width;
+			this.mainSceneShaderMaterial.uniforms.aaNdcOffset.value.y = 2*this.aaNDCOffset*yRand / this.height;
 		}
 
 		// generate new frame from main scene
@@ -83,10 +66,10 @@ function MFSViewer(targetDiv, width, height, objPath) {
 	this.id = nextID++;
 
 	// prepare renderer
-	this.renderer = new THREE.WebGLRenderer( { alpha: true, premultipliedAlpha: false } );
+	this.renderer = new THREE.WebGLRenderer( { alpha: true } );
 	this.renderer.setSize(this.width, this.height);
 	this.renderer.setPixelRatio(window.devicePixelRatio);
-	this.renderer.setClearColor(0xffffff, 0);
+	this.renderer.setClearColor(0x000000, 0);
 	if (this.renderer.getPrecision() == "highp") {
 		this.log("High Precision Floats are supported and will be used.");
 	}
@@ -219,7 +202,7 @@ function MFSViewer(targetDiv, width, height, objPath) {
 	// load quad for finalScene
 	this.mixQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), this.mixSceneShaderMaterial);
 	this.mixScene.add(this.mixQuad);
-	this.finalQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), new THREE.MeshBasicMaterial( { transparent: false } ));
+	this.finalQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), new THREE.MeshBasicMaterial( { transparent: true } ));
 	this.finalScene.add(this.finalQuad);
 
 	// attach the render-supplied DOM element
@@ -244,6 +227,7 @@ function MFSViewer(targetDiv, width, height, objPath) {
 	var guiOptions = {
 		"ViewerID(ReadOnly)": myself.id,
 		targetFrameCount: "64",
+		"aaNDCOffset[-x;x]": 0.5,
 		antiAliasing: true
 	};
 	var updateTargetFrameCount = function() {
@@ -253,13 +237,19 @@ function MFSViewer(targetDiv, width, height, objPath) {
 			myself.requestRender();
 		}
 	};
+	var updateaaNDCOffset = function() {
+		myself.aaNDCOffset = guiOptions["aaNDCOffset[-x;x]"];
+		myself.requestRender();
+	};
 	var updateRenderSettings = function () {
 		myself.mainSceneShaderMaterial.uniforms.antiAliasing.value = guiOptions.antiAliasing;
 		myself.requestRender();
 	}
 	updateTargetFrameCount();
 	updateRenderSettings();
+	updateaaNDCOffset();
 	this.gui.add(guiOptions, "ViewerID(ReadOnly)");
 	this.gui.add(guiOptions, "targetFrameCount").onChange(updateTargetFrameCount);
+	this.gui.add(guiOptions, "aaNDCOffset[-x;x]").onChange(updateaaNDCOffset);
 	this.gui.add(guiOptions, "antiAliasing", true).onChange(updateRenderSettings);
 }
